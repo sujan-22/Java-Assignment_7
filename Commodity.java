2ae9bd44ef08f694f2bc2f7d7f49bc0a7e862ece
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-01-12 23:38:07 -0500

    Update Commodity.java

--- Commodity.java
+++ Commodity.java
@@ -1,9 +1,31 @@
-//Sujan Rokad, 000882948
+/*
+ * Author:                 Sujan Rokad
+ * Authorship statement:   I, Sujan Rokad, 000882948, certify that this material is my original work.
+ *                         No other person's work has been used without due acknowledgment.
+ * Purpose:                Define the Commodity interface to represent commodity items in the game.
+ */
 
 package Assignment_7_000882948;
 
-// Define the Commodity interface
+/**
+ * The Commodity interface represents commodity items in the game.
+ * It defines methods to get the production cost and retail price of the commodity item.
+ *
+ * @author Sujan Rokad
+ */
 public interface Commodity {
-   double getProductionCost();   // returns the cost of producing the commodity item
-   double getRetailPrice();      // returns the retail price of the commodity item
+
+    /**
+     * Gets the production cost of the commodity item.
+     *
+     * @return The cost of producing the commodity item.
+     */
+    double getProductionCost();
+
+    /**
+     * Gets the retail price of the commodity item.
+     *
+     * @return The retail price of the commodity item.
+     */
+    double getRetailPrice();
 }
