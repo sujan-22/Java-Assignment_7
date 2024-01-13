86d03e2a6c2c0b8dc55975dc98f8aab17f46933e
Sujan <120612188+sujan-22@users.noreply.github.com>
2024-01-12 23:38:34 -0500

    Update Consumable.java

--- Consumable.java
+++ Consumable.java
@@ -1,9 +1,31 @@
-//Sujan Rokad, 000882948
+/*
+ * Author:                 Sujan Rokad
+ * Authorship statement:   I, Sujan Rokad, 000882948, certify that this material is my original work.
+ *                         No other person's work has been used without due acknowledgment.
+ * Purpose:                Define the Consumable interface to represent consumable items in the game.
+ */
 
 package Assignment_7_000882948;
 
-// Define the Consumable interface
+/**
+ * The Consumable interface represents consumable items in the game.
+ * It defines methods to get the calorie count and consumption method of the consumable item.
+ *
+ * @author Sujan Rokad
+ */
 public interface Consumable {
-    int getCalorieCount();      // returns the number of calories in the consumable item
-    String getConsumptionMethod();   // returns the method of consumption for the consumable item
+
+    /**
+     * Gets the number of calories in the consumable item.
+     *
+     * @return The calorie count of the consumable item.
+     */
+    int getCalorieCount();
+
+    /**
+     * Gets the method of consumption for the consumable item.
+     *
+     * @return The method of consumption for the consumable item.
+     */
+    String getConsumptionMethod();
 }
